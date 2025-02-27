01ccc0818d4b9c56517fbf4b78f5cc05bce7a626
[Dominick Sclafani] <[dssclafani@gmail.com]>
2025-02-27 16:54:41 -0500

    added bootstrap styling after signing in

--- food_for_family/index.php
+++ food_for_family/index.php
@@ -20,7 +20,7 @@ include('db.php');
     <div class="container mt-5">
         <?php if (!isset($_SESSION["username"])): ?> <!--checks if user is logged in -->
             <div id="auth-container">
-                <h2 class="text-center">Login or Register</h2>
+                <h2 class="text-center">Login or Register with us!</h2>
                 <div class="btn-group d-flex mb-3">
                     <button class="btn btn-primary w-50" onclick="showForm('login')">Login</button>
                     <button class="btn btn-secondary w-50" onclick="showForm('register')">Register</button>
@@ -31,11 +31,24 @@ include('db.php');
             </div>
         <?php else: ?>
             <!--display the logged in user-->
-            <div class="text-center">
-                <h1>Welcome, <?= htmlspecialchars($_SESSION["username"]); ?>!</h1>
-                <a href="logout.php" class="btn btn-danger">Logout</a>
+            <div class=" container text-center ">
+                <div class="row">
+                    <div class="col">
+                        <h1>Welcome, <?= htmlspecialchars($_SESSION["username"]); ?>!</h1>
+                        <a href="logout.php" class="btn btn-danger">Logout</a>
+                    </div>
+
+                </div>
             </div>
-        <?php endif; ?>
+        </div>
+        <!--placeholder for where posts will go-->
+        <div class="position-relative p-5 text-center text-muted bg-body border border-dashed rounded-5 col-12">
+            <h1 class="text-body-emphasis">Sorry for the wait!</h1>
+            <p class="col-lg-6 mx-auto mb-4">
+                We are working diligently to get our Webapp in your hands! Thank you for the patience.
+            </p>
+        </div>
+    <?php endif; ?>
     </div>
     <script src="script.js"></script>
 </body>
